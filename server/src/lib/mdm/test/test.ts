ffa64bc5ada25f15fdb74e6d462834098e25fd74
Yang Wei <961433139@qq.com>
2021-12-22 09:13:49 +0800

    移除CPP代码，MDM部分改为使用JSDom

--- server/src/lib/mdm/test/test.ts
+++ server/src/lib/mdm/test/test.ts
@@ -1,17 +1,10 @@
-import { readFileSync } from "fs";
 import { resolve } from "path";
-import { DOMParser } from "xmldom";
-import { MDMDocument } from "..";
-
 
 const xmlPath = resolve("./server/src/lib/mdm/test/fixture/ger_21marw2.mdd");
 // const xmlContent = readFileSync(xmlPath).toString();
 // const xmlParser = new DOMParser();
 // const doc = xmlParser.parseFromString(xmlContent);
 // const fstChld = doc.firstChild!;
-
-let doc = new MDMDocument(xmlPath);
-doc.load();
 
 console.log("end");
 
